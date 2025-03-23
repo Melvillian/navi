2b89571b12d839e345c6eb3c04f87ff0f67a02f4
Melvillian <melvillevt@gmail.com>
2025-03-23 18:28:44 -0400

    further refactor of notion logic into notion.rs.
    This is just further cleanup and making main.rs simple while all
    notion-related logic is in src/notion.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use chrono::{Duration, Utc};
+use chrono::Duration;
 use clap::Parser;
 use dotenv::dotenv;
-use log::{debug, info, trace};
-use navi::{
-    core::{
-        datatypes::{Block, ParsedNotionPage},
-        helpers::build_markdown_from_trees,
-    },
-    intelligence::assistant_flow,
-    notion::Notion,
-};
-use notion_client::NotionClientError;
-use std::{collections::HashSet, env, fs, path::Path};
+use log::{debug, info};
+use navi::{intelligence::assistant_flow, notion::Notion};
+use std::{env, fs, path::Path};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -43,8 +35,8 @@ async fn main() {
         info!(target: "notion", "Using cached prompt info from prompt_info.md");
         fs::read_to_string("prompt_info.md").unwrap()
     } else {
-        let parsed_pages = parse_last_edited(notion, dur).await.unwrap();
-        let prompt = to_prompt_text(parsed_pages).unwrap();
+        let parsed_pages = notion.parse_last_edited(dur).await.unwrap();
+        let prompt = Notion::to_prompt_text(parsed_pages).unwrap();
         if use_prompt_info_file {
             fs::write("prompt_info.md", &prompt).unwrap();
         }
@@ -56,63 +48,4 @@ async fn main() {
     info!(target: "notion", "Let's begin by asking Navi to start the retro, and see what Navi's response is...");
 
     assistant_flow(prompt_info).await.unwrap();
-}
-
-async fn parse_last_edited(
-    notion: Notion,
-    dur: Duration,
-) -> Result<Vec<ParsedNotionPage>, NotionClientError> {
-    info!(target: "notion", "Thanks for choosing Navi as your digital mentor! Navi will begin by analyzing your last {} {} of notes. This may take several minutes, depending on how dedicated a notetaker you are...", dur.num_days(), if dur.num_days() == 1 { "day" } else { "days" });
-    let cutoff = Utc::now() - dur;
-
-    let pages_edited_after_cutoff_date = notion.get_last_edited_pages(cutoff).await.unwrap();
-    info!(target: "notion", "retrieved {} Pages edited in the last {} days", pages_edited_after_cutoff_date.len(), dur.num_days());
-    info!(target: "notion", "From these Pages, Navi will fetch the notes it needs to guide you in reflecting on the past {} days", dur.num_days());
-
-    // TODO: idea: instead of storing the whole Block data, which is 95% worthless data, just strip out the
-    // text and id, store that in a struct, and use that to build the markdown
-    let mut parsed_pages = Vec::new();
-
-    let mut block_roots_duplicates_checker: HashSet<Block> = HashSet::new();
-    let mut expanded_blocks_duplicates_checker: HashSet<Block> = HashSet::new();
-    for page in pages_edited_after_cutoff_date {
-        debug!(target: "notion", "Page URL: {}", page.url);
-
-        let new_block_roots = notion
-            .get_page_block_roots(&page, cutoff, &mut block_roots_duplicates_checker)
-            .await
-            .unwrap();
-
-        if new_block_roots.len() > 0 {
-            debug!(target: "notion", "found {} new block roots for page: {}",  new_block_roots.len(), page.title);
-            let trees = notion
-                .expand_block_roots(new_block_roots, &mut expanded_blocks_duplicates_checker)
-                .await
-                .unwrap();
-
-            parsed_pages.push(ParsedNotionPage {
-                page_id: page.id,
-                title: page.title,
-                page_content: trees,
-            });
-        }
-    }
-
-    debug!(target: "notion", "retrieved {} pages with non-empty block roots", parsed_pages.len());
-    trace!(target: "notion", "the parsed pages look like:\n{:#?}", parsed_pages.iter().map(|p| (&p.page_id, p.page_content.iter().map(|t| (t.root().borrow_data().id.clone(), t.root().borrow_data().text.clone())).collect::<Vec<_>>())).collect::<Vec<_>>());
-
-    Ok(parsed_pages)
-}
-
-fn to_prompt_text(notion_pages: Vec<ParsedNotionPage>) -> Result<String, NotionClientError> {
-    let mut every_prompt_markdown = Vec::new();
-    for page in notion_pages {
-        let single_page_prompt_markdown = build_markdown_from_trees(page.page_content);
-        every_prompt_markdown.push(format!(
-            "Page Title: {}\n{}",
-            page.title, single_page_prompt_markdown
-        ));
-    }
-
-    Ok(every_prompt_markdown.join("\n\n"))
 }
--- src/notion.rs
+++ src/notion.rs
@@ -1,7 +1,10 @@
-use crate::core::datatypes::{Block, BlockID, Page, PageID};
+use crate::core::{
+    datatypes::{Block, BlockID, Page, PageID},
+    helpers::build_markdown_from_trees,
+};
 use chrono::{DateTime, Duration, Utc};
 use dendron::{Node, Tree};
-use log::{debug, error, trace};
+use log::{debug, error, info, trace};
 use notion_client::{
     endpoints::{
         blocks::retrieve::response::RetrieveBlockChilerenResponse,
@@ -20,6 +23,14 @@ pub struct Notion {
     client: Client,
 }
 
+/// A ParsedNotionPage represents a page that has been processed and contains its content as a tree of blocks.
+#[derive(Debug, Clone)]
+pub struct ParsedNotionPage {
+    pub page_id: PageID,
+    pub title: String,
+    pub page_content: Vec<Tree<Block>>,
+}
+
 impl Notion {
     pub fn new(token: String) -> Result<Self, NotionClientError> {
         let client = Client::new(token, None);
@@ -27,6 +38,77 @@ impl Notion {
             Ok(c) => Ok(Notion { client: c }),
             Err(e) => Err(e),
         }
+    }
+
+    /// Ingests and parsed the last edited pages in the Notion integration's workspace that have been
+    /// edited since the cutoff date
+    ///
+    /// # Returns
+    /// A `Result` containing a `Vec` of `ParsedNotionPage`s, which contain the page's ID, title, and content as a tree of blocks.
+    pub async fn parse_last_edited(
+        &self,
+        dur: Duration,
+    ) -> Result<Vec<ParsedNotionPage>, NotionClientError> {
+        info!(target: "notion", "Thanks for choosing Navi as your digital mentor! Navi will begin by analyzing your last {} {} of notes. This may take several minutes, depending on how dedicated a notetaker you are...", dur.num_days(), if dur.num_days() == 1 { "day" } else { "days" });
+        let cutoff = Utc::now() - dur;
+
+        let pages_edited_after_cutoff_date = self.get_last_edited_pages(cutoff).await.unwrap();
+        info!(target: "notion", "retrieved {} Pages edited in the last {} days", pages_edited_after_cutoff_date.len(), dur.num_days());
+        info!(target: "notion", "From these Pages, Navi will fetch the notes it needs to guide you in reflecting on the past {} days", dur.num_days());
+
+        // TODO: idea: instead of storing the whole Block data, which is 95% worthless data, just strip out the
+        // text and id, store that in a struct, and use that to build the markdown
+        let mut parsed_pages = Vec::new();
+
+        let mut block_roots_duplicates_checker: HashSet<Block> = HashSet::new();
+        let mut expanded_blocks_duplicates_checker: HashSet<Block> = HashSet::new();
+        for page in pages_edited_after_cutoff_date {
+            debug!(target: "notion", "Page URL: {}", page.url);
+
+            let new_block_roots = self
+                .get_page_block_roots(&page, cutoff, &mut block_roots_duplicates_checker)
+                .await
+                .unwrap();
+
+            if new_block_roots.len() > 0 {
+                debug!(target: "notion", "found {} new block roots for page: {}",  new_block_roots.len(), page.title);
+                let trees = self
+                    .expand_block_roots(new_block_roots, &mut expanded_blocks_duplicates_checker)
+                    .await
+                    .unwrap();
+
+                parsed_pages.push(ParsedNotionPage {
+                    page_id: page.id,
+                    title: page.title,
+                    page_content: trees,
+                });
+            }
+        }
+
+        debug!(target: "notion", "retrieved {} pages with non-empty block roots", parsed_pages.len());
+        trace!(target: "notion", "the parsed pages look like:\n{:#?}", parsed_pages.iter().map(|p| (&p.page_id, p.page_content.iter().map(|t| (t.root().borrow_data().id.clone(), t.root().borrow_data().text.clone())).collect::<Vec<_>>())).collect::<Vec<_>>());
+
+        Ok(parsed_pages)
+    }
+
+    /// Converts Navi's internal representation of a Vec of Page's content into a markdown
+    /// prompt that can be used to guide Navi in reflecting on the past duration's worth of notetaking.
+    ///
+    /// # Returns
+    /// A `Result` containing a `String` of prompt markdown.
+    pub fn to_prompt_text(
+        notion_pages: Vec<ParsedNotionPage>,
+    ) -> Result<String, NotionClientError> {
+        let mut every_prompt_markdown = Vec::new();
+        for page in notion_pages {
+            let single_page_prompt_markdown = build_markdown_from_trees(page.page_content);
+            every_prompt_markdown.push(format!(
+                "Page Title: {}\n{}",
+                page.title, single_page_prompt_markdown
+            ));
+        }
+
+        Ok(every_prompt_markdown.join("\n\n"))
     }
 
     /// Returns all `Page`s in the Notion integration's workspace that have been edited since the cutoff date.
@@ -244,6 +326,7 @@ impl Notion {
 
         Ok(())
     }
+
     /// Given a `Vec` of `Block`s (call these `Block`s "roots") that have been updated recently,
     /// return a `Vec` of `Tree`'s where each `Tree` contains the `Block` root and all of its descendants.
     /// We do this by recursively fetching the children of each root, and the children of those
